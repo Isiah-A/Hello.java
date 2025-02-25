ca05512b11e370a9f33284c4cd4b9403d6ad2a61
Isiah-A <isiaha478@gmail.com>
2025-02-24 19:51:37 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,48 @@
+Last login: Mon Feb 24 19:05:08 on ttys001
+isiah@Zipcoders-MacBook-Pro ~ % nano Yowza.jsh
+isiah@Zipcoders-MacBook-Pro ~ % nano Yowza.jsh
+isiah@Zipcoders-MacBook-Pro ~ % nano Hello.java
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  UW PICO 5.09                    File: Hello.java                    Modified  
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+^G Get Help  ^O WriteOut  ^R Read File ^Y Prev Pg   ^K Cut Text  ^C Cur Pos   
+^X Exit      ^J Justify   ^W Where is  ^V Next Pg   ^U UnCut Text^T To Spell  
